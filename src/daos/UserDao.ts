d43cfe88b5154987edd1488d0818389887e389e8
xpioneer <1286283280@qq.com>
2024-08-24 00:56:42 +0800

    add user dao

---
+++ src/daos/UserDao.ts
@@ -0,0 +1,100 @@
+import {
+  Equal,
+  Like,
+  Between,
+  FindManyOptions,
+  FindOptions,
+  In,
+} from "typeorm";
+import { Context } from '@/core/koa'
+import { Stock } from '@/entities/mysql/shares/stock'
+import { EMarket, EBLock } from '@/models/Stocks'
+import { getSharesManager, getSharesRepository, createSharesQueryBuilder } from '@/database/dbUtils';
+
+
+
+class StockDao {
+
+
+  async getCode(id: number) {
+    const stock = await getSharesRepository(Stock).findOne({
+      where: {
+        id
+      }
+    })
+    return stock
+  }
+
+  async getById(id: number) {
+    const stock = await getSharesRepository(Stock).findOne({
+      where: {
+        id
+      },
+      select: ['id', 'code', 'name']
+    })
+    return stock
+  }
+
+  async getByIds(ids: number[]) {
+    const stocks = await getSharesRepository(Stock).find({
+      where: {
+        id: In(ids)
+      }
+    })
+    // const stocks = await getSharesRepository(Stock).findByIds(ids, {
+    //   select: ['id', 'code', 'name']
+    // })
+    return stocks
+  }
+
+  async getByCode(code: string) {
+    const stock = await getSharesRepository(Stock).findOneBy({code: Equal(code)})
+    return stock
+  }
+
+  async getStockList(value: string, pageSize = 10) {
+    const list = await getSharesRepository(Stock).find({
+      where: [
+        { code: Like(`%${value}%`) },
+        { name: Like(`%${value}%`) }
+      ],
+      order: {
+        code: 'DESC'
+      },
+      take: pageSize
+    })
+    return list
+  }
+
+  async pages(offset = 1, size = 10, code?: string, name?: string, market?: EMarket, block?: EBLock) {
+    const where: FindManyOptions<Stock>['where'] = {}
+    if(code) {
+      where.code = Like(`%${code}%`)
+    }
+    if(name) {
+      where.name = Like(`%${name}%`)
+    }
+    if(market) {
+      where.market = Equal(market)
+    }
+    if(block) {
+      where.block = Equal(block)
+    }
+    const options: FindManyOptions<Stock> = {
+      skip: (offset > 0 ? (offset - 1) : 0) * size,
+      take: size,
+      order: { id: 'ASC'},
+      where: {}
+    }
+    const pages = await getSharesRepository(Stock).findAndCount(options)
+    return pages
+  }
+
+  async getBlocksCount(): Promise<{total: number, block: EBLock}> {
+    const counts = await createSharesQueryBuilder(Stock, 'groupByBlocks')
+      .select('count(*) as total, block').groupBy('block').execute()
+    return counts
+  }
+}
+
+export default new StockDao
